4a7538fdc7302a3a9ed35a2ecfa72fa673c14498
Fenton Travers <fenton.travers@gmail.com>
2022-05-01 17:20:46 -0700

    '.'

--- src/my_code/core.cljs
+++ src/my_code/core.cljs
@@ -5,16 +5,26 @@
    [cljsjs.web3]
    [cljs-web3.core :as web3]))
 
-(def wallet-2 "0x95A9bd206aE52C4BA8EecFc93d18EACDd41C88CC")
+(defonce w3
+  (web3/create-web3 "https://bsc-dataseed.binance.org/"))
 
-(def w3 (web3/create-web3 "https://bsc-dataseed.binance.org/"))
+(def data (atom {:nodes [{:node-url "https://bsc-dataseed.binance.org/"
+                          :name "Binance Smart Chain RPC"}]
+                 :wallets [{:nick-name "some router"
+                            :id "0x95A9bd206aE52C4BA8EecFc93d18EACDd41C88CC"}
+                           {:nick-name "another router"
+                            :id "0xcdd95e8738edc0733583ee484a3050a05ee87ff5"}]}))
 
-(defn getBalance [account-id web-3]
-  (.fromWei web-3 (.getBalance (. web-3 -eth) account-id)))
+(defn get-balance [account-id]
+  (get-balance account-id w3))
 
 (defn ui []
   [:div
-   [:p (str "Wallet ID: " wallet-2)]
-   [:p (str "Balance: " (getBalance wallet-2 w3))]])
+   [:div
+    (for [item (-> @data :wallets)]
+      ^{:key item}
+      [:div
+       [:div (str "balance: " (get-balance (:id item)))]
+       [:div (str "Nick Name: " (:nick-name item))]])]])
 
 (rdom/render ui (gdom/getElement "app"))
